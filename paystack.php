fa8b0dba9823f139cfaef684e65dab3a1d94ac5d
BrightAugustt <79414211+BrightAugustt@users.noreply.github.com>
2022-10-31 10:33:19 +0000

    aa

--- paystack.php
+++ paystack.php
@@ -17,21 +17,37 @@
         <label for="amount">Amount</label>
         <input type="tel" id="amount" name="amount" required />
     </div>
-    <div class="form-group">
-        <label for="first-name">First Name</label>
-        <input type="text" id="first-name" name="fname" />
-    </div>
-    <div class="form-group">
-        <label for="last-name">Last Name</label>
-        <input type="text" id="last-name" name="email"/>
-    </div>
     <div class="form-submit">
         <button type="submit" onclick="payWithPaystack()"> Pay </button>
     </div>
     </form>
 
 <script src="https://paystack.co/v1/inline.js"></script> 
-<script src="paystack.js"></script>
+<script>
+    const paymentForm = document.getElementById('paymentForm');
+paymentForm.addEventListener("submit", payWithPaystack, false);
+function payWithPaystack(e) {
+  e.preventDefault();
+
+  let handler = PaystackPop.setup({
+    key: 'pk_test_839cab168cfb81723db8ebef98f189d3bbc66f22', // Replace with your public key
+    email: document.getElementById("email-address").value,
+    amount: document.getElementById("amount").value * 100,
+    currency: "GHS",
+    ref: ''+Math.floor((Math.random() * 1000000000) + 1), // generates a pseudo-unique reference. Please replace with a reference you generated. Or remove the line entirely so our API will generate one for you
+    // label: "Optional string that replaces customer email"
+    onClose: function(){
+      alert('Window closed.');
+    },
+    callback: function(response){
+      let message = 'Payment complete! Reference: ' + response.reference;
+      alert(message);
+    }
+  });
+
+  handler.openIframe();
+}
+</script>
 </body>
 </html>
 
